885a164048812d57e7a2b88ab6a98d08b47c903e
JoleneMel <jolenemelanson33@gmail.com>
2022-08-31 13:43:15 -0400

    redoing the project w/new ways

---
+++ index.js
@@ -0,0 +1,20 @@
+//This is a set up for a redo of a old project, saw other ways to do it and I want to do it again but differently 
+// Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+
+// Programmatically subtract the value of the first element in the array from the value in the last element of the array (do not use numbers to reference the last 
+//element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
+
+// Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
+
+// Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+
+
+//Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
+
+// Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
+
+// Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
+
+// How do you access the last element of any array?
+
+// How do you access the first element of any array?
