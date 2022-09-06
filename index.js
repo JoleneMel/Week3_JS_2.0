9d44fcefa634a71b59fb0c7158f1ce1b9a64e544
JoleneMel <jolenemelanson33@gmail.com>
2022-09-06 12:48:44 -0400

    putting in very detailed notes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 /*Another way is to set up the array and then populate it with push */ 
 let ages2 = [];
-ages2.push([3, 9, 23, 64, 2, 8, 28, 93]);
+ages2.push(3, 9, 23, 64, 2, 8, 28, 93);
 
 /* To see that they have the same values you can console.log it */ 
 console.log("ages array below");
@@ -19,6 +19,7 @@ console.log(ages2);
 /*Arrays start off with 0s
 the length of an array is the amount of elements (which would be 8 elements, while the 0 based index system makes it so -1 is needed to get the last part)
 As you can see below we can use this logic to make a function */
+console.log("With just the logic")
 console.log(ages[ages.length - 1] - ages[0]);
 
 /* Functions use (parameters) a parameter is the named placeholder for whatever data you want to pass into the function. 
@@ -41,6 +42,7 @@ function subFirstEleLastEleConsole(array) {
 
 This is how you call function that already outputs the data to the console.log
 */
+console.log("Console.log function")
 subFirstEleLastEleConsole(ages);
 
 /*Return utilizing ES6 rules */
@@ -52,7 +54,7 @@ The return will provide that answer.
 
 This is how you call a function without a console.log
 */
-console.log(subFirstEleLastEleReturn(ages2));
+console.log("With the return function", + subFirstEleLastEleReturn(ages2));
 
 
 // Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
@@ -76,6 +78,18 @@ let total = 0;
 ages.forEach(item => total += item); 
 //Then we take the total and divide by the amount of elements within the array aka array/length
 console.log('Average is: ' + ' ' + total / ages.length, + "using the .forEach"); 
+/* Or a for loop or while loop */
+let sum = 0; 
+for(let i = 0; i < ages2.length; i++) {
+    sum += ages2[i];
+}//We need to do the average calulation outside of the loop 
+console.log("Using a for loop to find the average", + sum/ ages2.length);
+
+let sum2 = 0; 
+for(let i = ages.length - 1; i >= 0; i--) {
+    sum2 += ages2[i];
+}//We need to do the average calulation outside of the loop 
+console.log("Using a for loop to find the average", + sum2/ ages2.length);
 
 //Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
 
