d622cd79fccfead712f226d278a5b5c558890eb3
JoleneMel <jolenemelanson33@gmail.com>
2022-09-06 12:09:31 -0400

    putting in very detailed notes

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ console.log(ages2);
 //element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
 //Arrays start off with 0s
 //the length of an array is the amount of elements (which would be 8 elements, while the 0 based index system makes it so -1 is needed to get the last part)
-console.log(ages[ages.length - 1] - ages[0] );
+console.log(ages[ages.length - 1] - ages[0]);
+
+//This can also be made into a function 
+function subFirstEleLastEleConsole(array) {
+    console.log(array[array.length - 1] - array[0]);
+}
+//In the above one we have console.log being put to output the data, that works however return can build upon and be called within other functions 
+
+//This is how you call function that already outputs the data to the console.log
+subFirstEleLastEleConsole(ages);
+
+//Return 
+function subFirstEleLastEleReturn(array) {
+    return array[array.length - 1] - array[0];
+}
 // Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
 
 // Use a loop to iterate through the array and calculate the average age. Print the result to the console.
