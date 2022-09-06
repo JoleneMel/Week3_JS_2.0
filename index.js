31f3c6638a553eef8114a139f0f1893eda6bcf17
JoleneMel <jolenemelanson33@gmail.com>
2022-09-06 12:01:51 -0400

    putting in very detailed notes/ways to do so

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 //This is a set up for a redo of a old project, saw other ways to do it and I want to do it again but differently 
 // Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+//The above is one way to create an array
+
+//Another way is to set up the array and then populate it with push 
+let ages2 = [];
+ages2.push([3, 9, 23, 64, 2, 8, 28, 93]);
+
+//To see that they have the same values you can console.log it 
+console.log(ages);
+console.log(ages2);
 
 // Programmatically subtract the value of the first element in the array from the value in the last element of the array (do not use numbers to reference the last 
 //element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
-
+//Arrays start off with 0s
+//the length of an array is the amount of elements 
+console.log(ages[ages.length - 1] - ages[0] );
 // Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
 
 // Use a loop to iterate through the array and calculate the average age. Print the result to the console.
