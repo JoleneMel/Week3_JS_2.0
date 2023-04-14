ea4bee661244a6f5f07c34163026afef6856d6ab
JoleneMel <jolenemelanson33@gmail.com>
2023-04-14 10:14:41 -0400

    bleh

---
+++ index2.js
@@ -0,0 +1,10 @@
+//How to declare a variable, '' ""
+var String = "This is a string";
+
+let number = 3;
+
+//let and const 
+
+//null is an absence of an object 
+//undefined means it hasnt been properly explained to the computer 
+
